88f56a42126eae6bf68a3227e31d262c7c8e46ce
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2024-06-01 23:20:07 -0400

    Update models.R

--- models.R
+++ models.R
@@ -1,51 +1,38 @@
-#Importing packages
-packages<-c('tidyverse','modelr')
-lapply(packages, library, character.only=TRUE)
+#Import libraries
+```{r}
+pkg<-c('modelr','tidyverse')
+lapply(pkg,library,character.only=TRUE)
+```
 
-#Creating sample data
-x<-sample(-5:5,30,replace=TRUE)
-y<-round(rnorm(30),4)
+#Creating sample dataset to generate the model of
+```{r}
+#Creating a data frame to obtain a random distribution of x and y values
+#The plot the data with ggplot
+dat<-tibble(
+  x=runif(100,-5,5),
+  y=runif(100,20,50)
+)
 
-scores<-data.frame(x,y)
-
-#Plot the data
-
-plot<-ggplot(data=scores,mapping=aes(x,y))+
+plot<-ggplot(dat,aes(x,y))+
   geom_point()
+```
 
-#Linear enough so write model
-l_model<-function(b,m){
-  m*scores$x + b
+#Generating models for the data
+```{r}
+mdl<-tibble(
+  s=runif(100,-10,10),
+  i=runif(100,20,50)
+)
+
+lines<-ggplot(dat,aes(x,y))+
+  geom_abline(data=mdl,aes(intercept=i,slope=s),alpha=1/4)+
+  geom_point()
+```
+
+#To find the best model of them all, emply the RMSD
+```{r}
+rmsd<-function(dataset,model){
+  sqrt((mean(dataset$y-model$s))^2)
 }
-trendline=l_model(0.5,2)
-
-#Plot the trend model
-
-plot +
-  geom_abline(data=trendline)
-
-#Generate many models
-models<-tibble(x=runif(300,-5,5),y=runif(300,-2,3))
-#Plot many models
-plot+
-  geom_abline(data=models, aes(intercept=x,slope=y,
-                               alpha=1/4))
-# Root mean square deviation for model we created
-rms<-function(model_line,dataset){
-  r1<-(abs(scores$y -model_line))^2
-  sum=0
-  for (i in r1){
-    sum=sum + i
-  }
-  sqrt(sum)
-}
-rms(trendline,scores)
-
-#Distances for all the bad models using purrr
-distances<-function(dataset){
-  models %>%
-    purrr::map2_dbl(x,y,dataset)
-  
-}
-distances(scores)
+```
 
