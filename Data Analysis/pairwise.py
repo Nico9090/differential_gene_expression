8fe42367d4c7de295e693e1de535bc3c1ddaf18a
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2024-06-05 00:01:21 -0400

    Update pairwise.py

--- Data Analysis/pairwise.py
+++ Data Analysis/pairwise.py
@@ -4,10 +4,10 @@ options= '''red orange yellow green blue indigo violet'''.split(' ')
 
 chosen_colors=[]
 for color in options:
-        for x in range(len(options)-(1+options.index(color))):
-                if color!=options[x+1]:
+        for x in range(options.index(color),len(options)-1):
+                if color!=options[x]:
                         choice= input(f'{color} or {options[options.index(color)+x+1]}: ')
-                        if choice != color and choice !=options[x+1]:
+                        if choice != color and choice !=options[options.index(color)+1]:
                                 print('Error! Select one of the above options')
                                 continue
         #chosen_colors.append(choice)
