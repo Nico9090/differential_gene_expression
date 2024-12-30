fa5408f91cf10e0047625fd6c276f9b3bf7e1c14
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2024-12-30 16:12:00 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args) {
+        NeedlemanWunsch a1 = new NeedlemanWunsch("GCATGCG","GATTACA");
+        System.out.println(a1.get_seqs());
+    }
+}
