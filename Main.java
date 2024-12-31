34a4705f4a54842e89ea4573c078b5a9b332dbf0
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2024-12-31 11:45:49 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main{
     public static void main(String[] args) {
         NeedlemanWunsch a1 = new NeedlemanWunsch("GCATGCG","GATTACA");
         System.out.println(a1.get_seqs());
+        a1.genMatrix();
+        a1.showMatrix();
     }
 }
