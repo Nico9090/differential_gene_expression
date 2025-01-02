b9a570d9378d8b72e890f21f19c071ca09edb8a6
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2025-01-02 10:46:30 -0500

    Update NeedlemanWunsch.java

--- NeedlemanWunsch.java
+++ NeedlemanWunsch.java
@@ -4,12 +4,16 @@ public class NeedlemanWunsch{
     final int nrow;
     final int ncol;
     int [][] nm_matrix;
+    int match;
+    int mismatch;
     public NeedlemanWunsch(String seq1, String seq2){
         this.seq1=seq1;
         this.seq2=seq2;
         this.nrow = seq1.length();
         this.ncol = seq2.length();
         this.nm_matrix=new int[this.nrow + 2][this.ncol + 2];
+        this.match=1;
+        this.mismatch=-1;
     }
 
     public String get_seqs(){
@@ -34,6 +38,26 @@ public class NeedlemanWunsch{
         }
         
         System.out.println();
+        }
+    }
+    public void doAlgorithm(){
+        nm_matrix[1][1]=0;
+        nm_matrix[1][2]=0;
+        for (int num=1;num<=nrow;num++){
+            nm_matrix[1][num]=mismatch;
+            mismatch--;
+        }
+
+        for (int num=1;num<=nrow;num++){
+            nm_matrix[num][1]=match;
+            match--;
+        }
+        for (int start=0,num=2; start <= nrow && num <=nrow;start++, num++){
+            if (seq1.charAt(start)==seq2.charAt(start)){
+            nm_matrix[num][num]++;
+        }
+        
+
         }
     }
 }
