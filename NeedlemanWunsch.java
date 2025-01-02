7b1a791e37498142d22ccec319df6d00ddc36c0c
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2025-01-02 11:47:34 -0500

    Update NeedlemanWunsch.java

--- NeedlemanWunsch.java
+++ NeedlemanWunsch.java
@@ -9,9 +9,9 @@ public class NeedlemanWunsch{
     public NeedlemanWunsch(String seq1, String seq2){
         this.seq1=seq1;
         this.seq2=seq2;
-        this.nrow = seq1.length();
-        this.ncol = seq2.length();
-        this.nm_matrix=new int[this.nrow + 2][this.ncol + 2];
+        this.nrow = seq1.length()+2;
+        this.ncol = seq2.length()+2;
+        this.nm_matrix=new int[this.nrow][this.ncol];
         this.match=1;
         this.mismatch=-1;
     }
@@ -22,8 +22,8 @@ public class NeedlemanWunsch{
     }
 
     public int[][] genMatrix(){
-        for (int i = 0; i < this.nrow + 2; i++) {
-            for (int j = 0; j < this.ncol + 2; j++) {
+        for (int i = 0; i < this.nrow; i++) {
+            for (int j = 0; j < this.ncol; j++) {
                 nm_matrix[i][j] = 0;  // Example initialization, set your logic here if needed CHATGPT
         }
         }
@@ -34,7 +34,7 @@ public class NeedlemanWunsch{
     public void showMatrix(){
         for (int i = 0; i < this.nrow; i++) {
             for (int j = 0; j < this.ncol; j++) {
-                System.out.print(nm_matrix[i][j] + " ");
+                System.out.printf("%5d ",nm_matrix[i][j] );
         }
         
         System.out.println();
@@ -43,21 +43,32 @@ public class NeedlemanWunsch{
     public void doAlgorithm(){
         nm_matrix[1][1]=0;
         nm_matrix[1][2]=0;
-        for (int num=1;num<=nrow;num++){
+        for (int num=1,mismatch=-1;num< this.nrow;num++,mismatch--){
             nm_matrix[1][num]=mismatch;
-            mismatch--;
         }
 
-        for (int num=1;num<=nrow;num++){
+        for (int num=1,match=1;num< this.nrow;num++,match--){
             nm_matrix[num][1]=match;
-            match--;
         }
-        for (int start=0,num=2; start <= nrow && num <=nrow;start++, num++){
+        for (int start=0,num=2; start < this.nrow && num <this.nrow;start++, num++){
             if (seq1.charAt(start)==seq2.charAt(start)){
             nm_matrix[num][num]++;
         }
         
 
+        }
+    }
+    public void update(){
+        if(seq1.charAt(0)==seq2.charAt(0)){
+            int a=nm_matrix[1][1]+this.match;
+            int b=nm_matrix[1][2]+this.match;
+            int c=nm_matrix[2][1]+this.match;
+            nm_matrix[2][2]=Math.max(Math.max(a,b),c);
+            System.out.println("a : "+  a);
+            System.out.println("b : "+  b);
+            System.out.println("c : "+  c);
+            System.out.println("nm_matrix[1][1] : "+  nm_matrix[1][1] );
+            System.out.println("match : "+  this.match );
         }
     }
 }
