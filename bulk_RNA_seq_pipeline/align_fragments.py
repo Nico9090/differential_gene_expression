dffb32654ed5e8268abbf275b9a9df33d58003f0
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2025-03-14 14:53:54 -0400

    Update align_fragments.py

--- bulk_RNA_seq_pipeline/align_fragments.py
+++ bulk_RNA_seq_pipeline/align_fragments.py
@@ -7,7 +7,7 @@ def build_index(organism_genome,outfile_header):
 
 build_index("","genome") #add path to organism genome
 
-def align(forward,reverse,outfile_header):
+def align(forward,reverse,index_file_header):
   subprocess.run(["hisat2", "-x", index_file_header, "-1", forward, "-2", reverse, "-S", output_sam])
   return None
 align("","","output") #add forward and reverse fastq that make up the paired end reads
