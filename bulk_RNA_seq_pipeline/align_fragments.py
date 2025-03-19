fd0c9703f8f8366cedd14be5b9b6bda5f68b1feb
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2025-03-19 16:30:18 -0400

    Update align_fragments.py

--- bulk_RNA_seq_pipeline/align_fragments.py
+++ bulk_RNA_seq_pipeline/align_fragments.py
@@ -1,23 +1,47 @@
 import subprocess
+import os
+program_path="/path/to/programs" #Use in absence of exported path
+
+#Output is .ht2 files which represent indices for segments of the chosen genome
+#Only need to run once per genome
 def build_index(organism_genome,outfile_header):
-  subprocess.run(["hisat2-build", organism_genome, outfile_header])
-  print("Index files created!")
-  subprocess.run(["ls", outfile_header + "*"])
-  return None
+        subprocess.run([f"hisat2-build", organism_genome, outfile_header])
+        return "Complete!"
 
-build_index("","genome") #add path to organism genome
+#build_index("GCF_000001405.40_GRCh38.p14_genomic.fna","genome") #uncomment to run
 
-def align(forward,reverse,index_file_header):
-  subprocess.run(["hisat2", "-x", index_file_header, "-1", forward, "-2", reverse, "-S", output_sam])
-  return None
-align("","","output") #add forward and reverse fastq that make up the paired end reads
+#############################################################################
 
+#Output is .sam which represents sequence alignment
+fastq_path="/path/to/fastq/"
+index_file_path="/path/to/index_files/"
+def align(index_file_header,forward,reverse,output_sam): #forward and reverse are fastq files
+        subprocess.run(["hisat2", "-x", index_file_header, "-1", forward, "-2", reverse, "-S", output_sam])
+        return "Alignment Complete!"
+
+align(f"{index_file_path}genome",f"{fastq_path}S12_1.fq",f"{fastq_path}S12_2.fq",
+        "S12.sam")
+align(f"{index_file_path}genome",f"{fastq_path}S13_1.fq",f"{fastq_path}S13_2.fq",
+        "S13.sam")
+align(f"{index_file_path}genome",f"{fastq_path}S14_1.fq",f"{fastq_path}S14_2.fq",
+        "S14.sam")
+align(f"{index_file_path}genome",f"{fastq_path}S15_1.fq",f"{fastq_path}S15_2.fq",
+        "S15.sam")
+align(f"{index_file_path}genome",f"{fastq_path}S16_1.fq",f"{fastq_path}S16_2.fq",
+        "S16.sam")
+
+###############################################################################################
+#Need to create .bam files out of .sam to for gene counting
 def create_and_sort_BAM(SAM_file,outfile,sorted_outfile):
-  subprocess.run(["samtools view", "-Sb", SAM_file, ">", outfile])
-  subprocess.run(["samtools sort", "-o", sorted_outfile, outfile])
-  return None
+        subprocess.run(["samtools view", "-Sb", SAM_file, ">", outfile])
+        subprocess.run(["samtools sort", "-o", sorted_outfile, outfile])
+        return "BAM Generated!"
 
-create_and_sort_BAM("","","") #inpput the SAM,BAM,and,sorted BAM paths
+SAM_list=[] #add the names of the .SAM files here once created
+#for file in SAM_list:
+#       file_header=file[0:file.index(".")]
+#       create_and_sort_BAM(file,f"{file_header}.bam",f"{file_header}_sorted.bam")
+
 
 def generate_read_counts(gtf_outfile,sorted_bam):
   subprocess.run(["stringtie", "-o ", gtf_outfile, sorted_bam])
