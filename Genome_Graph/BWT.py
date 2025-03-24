33328a9136beead930e7f89a229c52a0a4375d60
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2025-03-24 14:07:59 -0400

    Update BWT.py

--- Genome_Graph/BWT.py
+++ Genome_Graph/BWT.py
@@ -5,8 +5,12 @@ tString=seq + "$"
 print(tString)
 
 def left_circular_shift(string):
-        all_left_circular_shifts=[string[1:]+string[0] for i in range(len(string))]
+        all_left_circular_shifts=[]
+        x=0
+        for i in range(len(string)):
+                all_left_circular_shifts.append(string[1:]+string[0])
+                string=string[1:]+string[0]
+                x+=1
         return all_left_circular_shifts
 
 print(left_circular_shift(tString))
-
