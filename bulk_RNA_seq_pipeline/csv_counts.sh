746489003e00b7d06270956bb06de6b2170edad3
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2025-04-16 14:18:41 -0400

    Update csv_counts.sh

--- bulk_RNA_seq_pipeline/csv_counts.sh
+++ bulk_RNA_seq_pipeline/csv_counts.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
+#SLURM OPTIONS_____________________________________________________
+#SBATCH -J "Gene Counts as CSV Generation"
+#SBATCH -o "SAM_transform.out"
+#SBATCH -e "SAM_transform.err"
+#___________________________________________________________________
+#Adjust environment if needed
 
+#eval "$(micromamba shell hook --shell bash)"
+#micromamba activate r-env
+#___________________________________________________________________
+#Main programs 
+Rscript sam_toCounts.R
+#Status check
+if [ $? -ne 0 ]; then
+  echo "Error from sam_toCounts.R execution" >&2
+  exit 1
+else
+  echo "Process Complete!"
+fi
+
+
+#__________________________________________________________________
