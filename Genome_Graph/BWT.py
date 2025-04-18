260ccd63f92b50fc9cff8936dd4240046ea3353c
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2025-04-18 14:19:13 -0400

    Update BWT.py

--- Genome_Graph/BWT.py
+++ Genome_Graph/BWT.py
@@ -1,28 +1,32 @@
 #!/usr/bin/env python3
 import pandas as pd
 #BW transform__________________________________________
-seq="GACTATATCCTAAATACCCGCACCATTACCGACACCCGTGGCCCAAGCAG"
-tString=seq + "$"
-print(tString)
-
-def left_circular_shift(string):
+class BWT:
+    def __init__(self,string):
+        self.string=string + "$"
+    def left_circular_shift(self):
         all_left_circular_shifts=[]
         x=0
-        for i in range(len(string)):
-                all_left_circular_shifts.append(string[1:]+string[0])
-                string=string[1:]+string[0]
-                x+=1
+        temp_string=self.string #use temp variable to avoid permanently changing self.string
+        for i in range(len(self.string)):
+            all_left_circular_shifts.append(temp_string[1:]+temp_string[0])
+            temp_string=temp_string[1:]+temp_string[0]
+            x+=1
         return all_left_circular_shifts
 
-lc_shifts=left_circular_shift(tString)
+    def BWT_data_frame(self):
+        lc_shifts=self.left_circular_shift()
+        sorted_lc_shifts=sorted(lc_shifts)
+        bwt = ''.join([shift[-1] for shift in sorted_lc_shifts])
+        #BWT table
+        data={
+            'Circular Shifts': lc_shifts,
+            'Sorted Circular Shifts': sorted_lc_shifts,
+            'BWT': [bwt]*len(lc_shifts)
+        }
+        df=pd.DataFrame(data)
+        return df
+seq="GACTATATCCTAAATACCCGCACCATTACCGACACCCGTGGCCCAAGCAG"
 
-sorted_lc_shifts=sorted(lc_shifts)
-bwt = ''.join([shift[-1] for shift in sorted_lc_shifts])
-#BWT table
-data={
-    'Circular Shifts': lc_shifts,
-    'Sorted Circular Shifts': sorted_lc_shifts,
-    'BWT': [bwt]*len(lc_shifts)
-}
-df=pd.DataFrame(data)
-print(df)
+data=BWT(seq)
+print(data.BWT_data_frame())
