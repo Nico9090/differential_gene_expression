54ad255ed81e0c01b91bd546efa0e8ef73c7b12e
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2025-04-23 10:36:51 -0400

    Update make_counts.R

--- pipeline_scripts/R/make_counts.R
+++ pipeline_scripts/R/make_counts.R
@@ -1,26 +1,17 @@
-#Libraries#_______________________________________________________________________
+#Obtain counts
+#==================================================================================
 library(Rsubread)
-#_________________________________________________________________________________
-gen_Counts<-function(
-  sam_file,
-  gtf_file){
+#=================================================================================
+gen_Counts<-function(sam_file,gtf_file,csv_file){
         SAM<-sam_file
         countMatrix<-featureCounts(files=SAM,
-                                   annot.inbuilt = NULL,
-                                   annot.ext= gtf_file
+                                   annot.inbuilt=NULL,
+                                   annot.ext=gtf_file,
+                                   isGTFAnnotationFile = TRUE,
+                                   isPairedEnd = TRUE
                                 )
         counts<-countMatrix$counts
         write.csv(counts,
-                  file = paste0(sub("\\..*$",
-                                    "",
-                                    sam_file[1]),
-                                "auto_counts.csv"),
+                  file = csv_file,
                   row.names = TRUE)
-}
-
-files<-list() #add the names of the files
-gtf<-"" #add gtf file
-for(file in files){
-  gen_Counts(file,
-             gtf)
 }
