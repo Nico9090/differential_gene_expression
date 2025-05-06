c7efaeb36c2a377b96d80e73fcabdaee36be9bcc
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2025-05-06 14:52:09 -0400

    Update SRA_to_fastq.sh

--- pipeline_scripts/shell/single_cell/SRA_to_fastq.sh
+++ pipeline_scripts/shell/single_cell/SRA_to_fastq.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 module load fasterq-dump
+
 INDIR=""
 OUTDIR=""
 cd $INDIR
 SRR_file=""
+prefetch --output-directory $OUTDIR $SRR_file
 fasterq-dump --outdir $OUTDIR --split-3 $SRR_file
