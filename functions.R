6f5667fa95717c41bd01809de00e2f8324e0bc09
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2025-07-31 09:19:11 -0400

    Update functions.R

--- functions.R
+++ functions.R
@@ -1,22 +1,28 @@
 #<---Functions--->
 packages<-c("tidyverse","limma","clusterProfiler")
-lapply(packages,base::library,character.only=TRUE)
+base::lapply(packages,base::library,character.only=TRUE)
 #<---limma
 limma_fit_for_deg<-function(counts,
                             design,
                             meta_data,
                             sample_identifier_by){
   #<---sample_identifier_by= condition or column 2 of meta data
+  
   colnames(design)<-levels(meta_data[[sample_identifier_by]])
+  
   fit<-limma::lmFit(as.matrix(log2(counts+1)),design)
+  
   #<--change PLDF - WTF when needed --->
   contrast_matrix <- limma::makeContrasts(PLDF - WTF,
                                    levels = design)
   fit2 <- limma::contrasts.fit(fit, contrast_matrix)
   fit2 <- limma::eBayes(fit2)
   deg_table <- limma::topTable(fit2, adjust = "fdr", number = Inf)
+  
   return(deg_table)
 }
+
+#<--gene set enrichment analysis
 gseago_fn<-function(deg_table,gene_col_name,fc_col,organism){
   library(clusterProfiler)
   library(enrichplot)
@@ -37,6 +43,8 @@ gseago_fn<-function(deg_table,gene_col_name,fc_col,organism){
                pAdjustMethod = "none")
   return(list(go_obj=gse))
 }
+
+#<---obtain affymetrix gene names
 convert_affy_id.fn<-function(gene.ids,affy.org.db){
   library(mouse4302.db)
   library(rat2302.db)
@@ -48,6 +56,7 @@ convert_affy_id.fn<-function(gene.ids,affy.org.db){
   
 }
 
+#<---DEG table using DESeq2
 DESeq2.fn<-function(raw.counts,meta.data,design){
   dds<-DESeqDataSetFromMatrix(countData=raw.counts,
                               colData = meta.data,
