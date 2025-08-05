487d2235b52254e742915a0735fee007b4cdc101
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2025-08-05 08:59:20 -0400

    Update functions.R

--- functions.R
+++ functions.R
@@ -1,33 +1,30 @@
 #<---Functions--->
-packages<-c("tidyverse","limma","clusterProfiler")
-base::lapply(packages,base::library,character.only=TRUE)
+packages<-c("tidyverse","limma","clusterProfiler",
+            "plotly","DOSE","enrichplot","maEndToEnd","DESeq2"
+            )
+lapply(packages,base::library,character.only=TRUE)
 #<---limma
 limma_fit_for_deg<-function(counts,
                             design,
                             meta_data,
                             sample_identifier_by){
   #<---sample_identifier_by= condition or column 2 of meta data
-  
   colnames(design)<-levels(meta_data[[sample_identifier_by]])
-  
   fit<-limma::lmFit(as.matrix(log2(counts+1)),design)
-  
   #<--change PLDF - WTF when needed --->
-  contrast_matrix <- limma::makeContrasts(PLDF - WTF,
+  contrast_matrix <- limma::makeContrasts(KO - WT,
                                    levels = design)
   fit2 <- limma::contrasts.fit(fit, contrast_matrix)
   fit2 <- limma::eBayes(fit2)
   deg_table <- limma::topTable(fit2, adjust = "fdr", number = Inf)
-  
   return(deg_table)
 }
-
-#<--gene set enrichment analysis
 gseago_fn<-function(deg_table,gene_col_name,fc_col,organism){
   library(clusterProfiler)
   library(enrichplot)
   library(ggplot2)
   library(org.Mm.eg.db)
+  library(org.Hs.eg.db)
   geneList <- deg_table[[fc_col]]
   names(geneList) <- deg_table[[gene_col_name]]
   gse <- gseGO(geneList=sort(geneList,
@@ -44,19 +41,20 @@ gseago_fn<-function(deg_table,gene_col_name,fc_col,organism){
   return(list(go_obj=gse))
 }
 
-#<---obtain affymetrix gene names
-convert_affy_id.fn<-function(gene.ids,affy.org.db){
+convert_id.fn<-function(gene.ids,affy.org.db){
   library(mouse4302.db)
   library(rat2302.db)
   library(pd.rat230.2)
-  df<-select(affy.org.db,
+  library(hgu133plus2.db)
+  library(hugene10stv1cdf)
+  library(hugene10sttranscriptcluster.db)
+  library(org.Hs.eg.db)
+  df<-AnnotationDbi::select(affy.org.db,
              keys=gene.ids,
              columns=c("SYMBOL","ENTREZID","GENENAME"))
   return(df)
   
 }
-
-#<---DEG table using DESeq2
 DESeq2.fn<-function(raw.counts,meta.data,design){
   dds<-DESeqDataSetFromMatrix(countData=raw.counts,
                               colData = meta.data,
